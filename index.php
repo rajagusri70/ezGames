88977b64fb28bfd0f71fb328d0e1c703223c9ba2
fadhilpratama4 <fadhilpratama08@gmail.com>
2017-11-22 05:49:39 +0700

    tittle doanggggg

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ if(isset($accessToken)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Games Zone A Games Category Flat Bootstrap responsive Website Template | Index :: w3layouts</title>
+<title>EasyGames Shop SI3803 | Home</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Games Zone Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
