35c36701815a5042bbc191d1471242d10439d873
Mindha <mindhaningrum11@gmail.com>
2017-11-21 17:53:34 +0700

    updated

--- index.php
+++ index.php
@@ -53,7 +53,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<!-- Navbar -->
 		<nav class="navbar navbar-default">
 			<div class="container">
-
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 						<span class="sr-only">Toggle navigation</span>
@@ -63,7 +62,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					</button>
 					<a  href="index.html"><h1>Games <img src="images/c1.png" alt=" " /> Zone</h1></a>
 				</div>
-
 				<div id="navbar" class="navbar-collapse collapse">
 					<ul class="nav navbar-nav navbar-right">
 						<li class="hover-effect"><a href="index.php">Home</a></li>
